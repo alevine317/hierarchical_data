2f307bddcbdc552a526ee008c172b20756c48477
Austin Levine <austinlevine4@gmail.com>
2019-10-04 13:35:16 -0400

    fixed requirements

--- mmpptt/hierarchical_data/hierarchical_data/models.py
+++ mmpptt/hierarchical_data/hierarchical_data/models.py
@@ -3,8 +3,16 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+type_of_choices = (
+    ('folder', 'Folder'),
+    ('file', 'File'),
+)
+
+
 class File(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
+    type_of = models.CharField(
+        max_length=6, choices=type_of_choices, default='file')
     parent = TreeForeignKey('self', on_delete=models.CASCADE,
                             null=True, blank=True, related_name='children')
 
